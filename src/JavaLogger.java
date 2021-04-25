b079b70f11d61aea59abebc863f6a35b4ceb057b
Aldo Vargas <aldov500@Aldos-MacBook-Pro.local>
2021-04-25 17:53:08 -0500

    Declarations

---
+++ src/JavaLogger.java
@@ -0,0 +1,17 @@
+
+public class JavaLogger {
+
+	JavaLogger(){
+		
+	}
+	
+	private void PrepareRecord() {
+		
+	}
+	
+	public void SaveLog() {
+		// Guarda en un archivo el log
+		PrepareRecord();
+	}
+
+}
---
+++ src/JavaLoggerDriver.java
@@ -0,0 +1,9 @@
+
+public class JavaLoggerDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
